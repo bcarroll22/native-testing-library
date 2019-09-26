742be60d57d60b1cf21ceeaed60ba45fc53e2c86
Sophie Au <SophieAu@user.noreply.github.com>
2019-09-26 18:21:10 +0200

    Add typings

--- typings/events.d.ts
+++ typings/events.d.ts
@@ -32,8 +32,9 @@ export declare class NativeTestEvent {
 
 export type FireFunction = (element: NativeTestInstance, event: NativeTestEvent) => boolean;
 export type FireObject = {
-  [K in EventType]: (element: NativeTestInstance, options?: {}) => boolean
+  [K in EventType]: (element: NativeTestInstance, options?: {}) => boolean;
 };
 
 export const getEventHandlerName: (key: string) => string;
 export const fireEvent: FireFunction & FireObject;
+export const asyncFireEvent: Promise<FireFunction & FireObject>;
